6be722e40dd2a0fd2b32528a147fa0bacf1ec9c8
Bloodbath1350 <147150162+Bloodbath1350@users.noreply.github.com>
2024-05-26 15:27:42 +0300

    пп

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,12 @@
 import readlineSync from 'readline-sync';
 
-export default function greeting() {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
 
-  const name = readlineSync.question('May I have your name? ');
+  const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
 
   return name;
-}
+};
+
+export default greeting;
